9a378e6dd30a3a058e1d0e7432713f515d661006
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-05-01 00:10:05 +0000

    Task 4 done

--- fb_io.mjs
+++ fb_io.mjs
@@ -15,18 +15,64 @@ console.log('%c fb_io.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
+    
+import { initializeApp }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+    import { getAuth, GoogleAuthProvider, signInWithPopup }
 
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+//Variables
 
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
 export { 
-    fb_initialise };
+    fb_initialise,fb_authenticate };
 
 function fb_initialise() {
-    console.log('%c fb_initialise(): ', 
-                'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const FB_CONFIG = {
+    apiKey: "AIzaSyAC9lbREKwJJ95pZUJ7Wy3hI_QfivE2a34",
+    authDomain: "comp-firebaseskills.firebaseapp.com",
+    projectId: "comp-firebaseskills",
+    storageBucket: "comp-firebaseskills.firebasestorage.app",
+    messagingSenderId: "634491601796",
+    appId: "1:634491601796:web:1c48be8af741f25bd353d1"
+  };
+
+  // Initialize Firebase
+  console.log(FB_CONFIG)
+  const FB_APP = initializeApp(FB_CONFIG);
+  console.log(FB_APP);
+  const FB_DATABASE = getDatabase(FB_APP);
+  console.log(FB_DATABASE);
+
+}
+
+function fb_authenticate() {
+    console.log("Fb_Authenitcate")
+
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+    // The following makes Google ask the user to select the account
+PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+        console.log("Authenticated sucessfully!")
+    })
+    .catch((error) => {
+        console.log(error)
+        });
 }
 
 /**************************************************************/
--- main.mjs
+++ main.mjs
@@ -12,10 +12,16 @@ console.log('%c main.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the constants & functions required from fb_io module
-import { fb_initialise }
+import { fb_initialise, fb_authenticate }
     from './fb_io.mjs';
     window.fb_initialise   = fb_initialise;
- 
+    window.fb_authenticate = fb_authenticate;
+    
+import { initializeApp }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
 /**************************************************************/
 // index.html main code
 /**************************************************************/
